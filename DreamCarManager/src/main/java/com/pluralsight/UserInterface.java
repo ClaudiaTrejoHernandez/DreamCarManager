b5db2296a3e12eed10a2f25098cbdfd64927e860
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-16 13:45:55 -0700

    Updated User Interface Class
    -Added empty methods to set up the user interface
    -will continue to build the methods

--- DreamCarManager/src/main/java/com/pluralsight/UserInterface.java
+++ DreamCarManager/src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,70 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+
+    public static Scanner read = new Scanner(System.in);
+    private Dealership dealership;
+
+    public UserInterface() {
+        init();
+    }
+
+    private void init() {
+        DealershipFileManager dfm = new DealershipFileManager();
+        this.dealership = dfm.getDealership();
+    }
+
+    public void display() {
+
+    }
+
+    public int displayMenu() { //Helper method to display menu. Can be called in other methods
+
+    }
+
+    private void displayAllVehicles(List<Vehicle> vehicles) {
+
+    }
+
+    private void processGetByPriceRequest() {
+
+    }
+
+    private void processGetByMakeModelRequest() {
+
+    }
+
+    private void processGetByYearRequest() {
+
+    }
+
+    private void processGetByColorRequest() {
+
+    }
+
+    private void processGeyByMileageRequest() {
+
+    }
+
+    private void processGetByVehicleTypeRequest() {
+
+    }
+
+    private void processGetAllVehiclesRequest() {
+
+    }
+
+    private void processAddVehicleRequest() {
+
+    }
+
+    private void processRemoveVehicleRequest() {
+        
+    }
 }
+
+
+
